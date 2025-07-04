f41e78fd523dbf39d26bb34626eec13424d82aa2
Jan-Paul-Langenfurth <jan-paul.langenfurth@outlook.de>
2025-07-04 15:38:29 +0200

    add support for lsp and a script to install them

---
+++ install_lsps.sh
@@ -0,0 +1,8 @@
+npm install -g intelephense stylelint-lsp typescript typescript-language-server
+
+if [[ $OSTYPE == 'darwin'* ]]; then
+	brew install tflint
+else
+	curl -s https://raw.githubusercontent.com/terraform-linters/tflint/master/install_linux.sh | bash
+fi
+
